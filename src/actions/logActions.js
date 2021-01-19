5f33fece924ae1114d2e4207b32fd887e696e75b
jim schuman <jim@jims-iMac.local>
2021-01-19 13:31:45 -0600

    add log functinality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import {GET_LOGS, SET_LOADING, LOGS_ERROR} from "./types";
+import {GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG} from "./types";
 
 export const getLogs = () => async dispatch =>{
 
@@ -13,6 +13,37 @@ export const getLogs = () => async dispatch =>{
 
         dispatch({
             type: GET_LOGS,
+            payload: data
+        });
+
+    } catch(error) {
+        //console.log(error);
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.data
+        });
+    }
+};
+
+export const addLog = (log) => async dispatch =>{
+
+    try {
+
+        setLoading();
+
+        const res = await fetch('/logs', {
+            method: 'POST',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        //console.log(res.data);
+        const data = await res.json();
+        //console.log(data);
+
+        dispatch({
+            type: ADD_LOG,
             payload: data
         });
 
